4e152a750ffdd9c2e296854cc805abf749c6e011
Lucas Patrício <lucas.patricio@ostenmoove.com.br>
2025-08-10 21:07:45 -0300

    Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,9 @@
-import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Typography } from '@mui/material'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
@@ -18,9 +17,9 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+      <Typography color="text.primary" fontWeight="semiBold">
+        Desenvolvendo um novo portfólio
+      </Typography>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
