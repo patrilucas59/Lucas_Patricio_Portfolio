0e7d15e7f34a6d38b7e03febbc8852352ef5a21c
Lucas Patrício <lucas.patricio@ostenmoove.com.br>
2025-09-08 09:00:06 -0300

    refact: Removing Avatar photo

--- src/pages/Home/sections/Hero/index.tsx
+++ src/pages/Home/sections/Hero/index.tsx
@@ -1,5 +1,4 @@
 
-import Avatar from '../../../../assets/images/Foto-de-Perfil.jpeg';
 import DownloadIcon from '@mui/icons-material/Download';
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import { StyledButton } from '../../../../components/Button';
@@ -26,25 +25,11 @@ const Hero = () => {
     alignItems: 'center',
   }))
 
-  const StyledImg = styled('img')(({ theme }) => ({
-    width: '100%',
-    maxWidth: '60%',
-    marginTop: '5%',
-    borderRadius: '50%',
-    border: `1px solid ${theme.palette.primary.contrastText}`
-  }))
-
-
   return (
     <>
       <StyledHero>
         <Container maxWidth='lg'>
           <Grid container spacing={2} justifyContent="center" alignItems="center">
-            <Grid>
-              <Box position='relative' textAlign='center' sx={{ margin: '0 auto' }}>
-                <StyledImg src={Avatar} alt="Foto-de-Perfil" />
-              </Box>
-            </Grid>
             <Grid>
               <Typography color='primary.contrastText' variant='h1' textAlign='center'>Lucas Patrício</Typography>
               <Typography color='primary.contrastText' variant='h2' textAlign='center'>Front-end Developer</Typography>
