41704c0912cc2053938da2373b9206937588e853
Lucas Patrício <lucas.patricio@ostenmoove.com.br>
2025-09-09 12:02:23 -0300

    fix: Position of LanguageSwitcher on Mobile screen

--- src/components/NavBar/index.tsx
+++ src/components/NavBar/index.tsx
@@ -24,7 +24,7 @@ const NavBar: React.FC<NavBarProps> = ({ onNavigate }) => {
 
   const StyledToolbar = styled(Toolbar)(() => ({
     display: 'flex',
-    justifyContent: isMobile ? 'left' : 'space-evenly',
+    justifyContent: isMobile ? 'space-between' : 'space-around',
     alignItems: 'center',
   }));
 
@@ -116,7 +116,7 @@ const NavBar: React.FC<NavBarProps> = ({ onNavigate }) => {
             </MenuItem>
           </>
         )}
-        <Box sx={{ backgroundColor: '#d6dee2', border: " 3px solid #0288d1", borderRadius: '5%', transition: 'all 0.5s ease' }}>
+        <Box sx={{ display: 'flex', alignItems: 'end', backgroundColor: '#d6dee2', border: " 3px solid #0288d1", borderRadius: '5%', transition: 'all 0.5s ease' }}>
           <LanguageSwitcher />
         </Box>
       </StyledToolbar>
