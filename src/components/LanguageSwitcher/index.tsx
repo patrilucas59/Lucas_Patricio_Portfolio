537b57e208af2305f8a9a480d9210335d6daa663
unknown <patriciolucas059@gmail.com>
2025-10-29 21:07:08 -0300

    feat(LanguageSwitcher): 2 new images of Brazil and USA flags]

--- src/components/LanguageSwitcher/index.tsx
+++ src/components/LanguageSwitcher/index.tsx
@@ -1,7 +1,9 @@
-import { Button, ButtonGroup, Menu, MenuItem } from "@mui/material";
+import { Avatar, Button, ButtonGroup, Menu, MenuItem } from "@mui/material";
 import { useState } from "react";
 import { useTranslation } from 'react-i18next';
 import PublicIcon from '@mui/icons-material/Public';
+import BrazilFlag from '../../assets/images/brazil-.png';
+import UsaFlag from '../../assets/images/united-states-of-america.png';
 
 const LanguageSwitcher = () => {
   const { i18n } = useTranslation();
@@ -39,8 +41,22 @@ const LanguageSwitcher = () => {
         anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
         transformOrigin={{ vertical: 'top', horizontal: 'right' }}
       >
-        <MenuItem onClick={() => changeLanguage("en")}>🇺🇸 EN</MenuItem>
-        <MenuItem onClick={() => changeLanguage("pt-br")}>🇧🇷 PT-BR</MenuItem>
+        <MenuItem onClick={() => changeLanguage("en")}>
+          <Avatar
+            alt="USA"
+            src={UsaFlag}
+            sx={{ width: 28, height: 28, mr: 1 }} 
+          />
+          EN
+        </MenuItem>
+        <MenuItem onClick={() => changeLanguage("pt-br")}>
+          <Avatar
+            alt="Brazil"
+            src={BrazilFlag} 
+            sx={{ width: 28, height: 28, mr: 1 }}
+          />
+          PT-BR
+        </MenuItem>
       </Menu>
     </>
   )
